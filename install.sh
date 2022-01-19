70e7df30c4dbc8a77e031812825a33d2ac26aeab
zhougaojun <zhougaojun@dxy.cn>
2022-01-19 15:39:32 +0800

    review

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ! -e /usr/bin/python3 ];then
    echo 'Error: python3 is not installed.'
+   exit 1
 fi
 wget https://github.com/telzhou618/x-tools/releases/download/v0.1/x-tools-0.1.tar.gz
 pip3 install x-tools-0.1.tar.gz
